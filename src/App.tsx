16818c22552a136e216871e7308a04af55764fd8
MRDC28 <marianadanhrocha@gmail.com>
2025-12-11 00:18:06 -0300

    Create main App component for FutGol Simulator

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MatchEngine from "./components/MatchEngine";
+import TransferSystem from "./components/TransferSystem";
+import Dashboard from "./components/Dashboard";
+import AgeSystem from "./components/AgeSystem";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>⚽ FutGol Simulator</h1>
+
+      <AgeSystem />
+      <Dashboard />
+      <TransferSystem />
+      <MatchEngine />
+    </div>
+  );
+}
