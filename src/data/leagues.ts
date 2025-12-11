b0070ab38e4e55f6f8f129450bdbbd1a3339d406
MRDC28 <marianadanhrocha@gmail.com>
2025-12-11 00:20:08 -0300

    Add leagues data for various football leagues

---
+++ src/data/leagues.ts
@@ -0,0 +1,7 @@
+export const leagues = {
+  "La Liga": ["Real Madrid", "Barcelona", "Atlético Madrid", "Valencia", "Sevilla"],
+  "Brasileirão Série A": ["Flamengo", "Palmeiras", "São Paulo", "Corinthians", "Grêmio"],
+  "Brasileirão Série B": ["Sport", "Ceará", "Santos", "Chapecoense", "Botafogo-SP"],
+  "Premier League": ["Manchester City", "Arsenal", "Chelsea", "Liverpool", "Manchester United"],
+  Bundesliga: ["Bayern", "Dortmund", "Leipzig", "Leverkusen", "Stuttgart"]
+};
