cd6851e3bdcaaf44855795ebc55948b66af7a79a
MRDC28 <marianadanhrocha@gmail.com>
2025-12-11 00:23:13 -0300

    Add player object with initial properties

---
+++ src/data/player-system.ts
@@ -0,0 +1,16 @@
+export const player = {
+  name: "",
+  age: 16,
+  country: "",
+  league: "",
+  team: "",
+  ger: 60,
+  stats: {
+    matches: 0,
+    goals: 0,
+    assists: 0,
+    yellow: 0,
+    red: 0,
+    ratingAvg: 0
+  }
+};
